a2a8d35f165009a976a74d4aeae2eabac9c69410
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-09-18 13:52:40 +0530

    feat(encryptedPassword):improves security while storing the password into database

--- app/Http/Controllers/FundooController.php
+++ app/Http/Controllers/FundooController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\fundooRegister;
 use Illuminate\Http\Request;
-class FundooController extends Controller
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
+class fundooController extends Controller
 {
     
     public function create(Request $request)
@@ -13,9 +17,24 @@ class FundooController extends Controller
         $users->firstname = $request->input('firstname');
         $users->lastname = $request->input('lastname');
         $users->email = $request->input('email');
-        $users->password = $request->input('password');
-
+        $users->password = md5($request->input('password'));
+       
         $users->save();
-        return response()->json($users);
+        return response()->json(['status'=>200,'message'=>'Registered Sucessfully','data'=>$users]);
+    }
+
+    public function login(Request $request){
+    
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $encrypted =md5($password);
+        
+        $result = DB::select('SELECT id FROM demousers WHERE email = ? AND password = ?',[$email,$encrypted]);
+        if (count($result)>0) {
+            return response()->json(['status'=>200,'message'=>'Login Successfully!!']);
+        } else {
+            return response()->json(['status'=>401,'message'=>'Email Not registered']);
+        }
+        
     }
 }
